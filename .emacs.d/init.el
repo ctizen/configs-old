48fcec9aea732c32a03e548c6fab61aee0517b3b
Oleg Klimenko <o.klimenko@2gis.ru>
2016-09-11 22:54:35 +0700

    Add email support for emacs using mu4e

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -318,3 +318,76 @@ That is, a string used to represent it on the tab bar."
 
 (global-set-key [M-s-left] 'tabbar-backward)
 (global-set-key [M-s-right] 'tabbar-forward)
+
+;; ------------------------------------------------------------------------
+;; mu4e mailer setup
+
+(add-to-list 'load-path "~/.emacs.d/mu4e")
+(require 'mu4e)
+(setq mu4e-mu-binary (concat
+                      "~/.emacs.d/mu4e/mu-binary-"
+                      (if (eq system-type 'darwin) "mac" "linux")))
+
+;; default
+(setq mu4e-maildir "~/Mail")
+(setq mu4e-drafts-folder "/Drafts")
+(setq mu4e-sent-folder   "/Sent")
+(setq mu4e-trash-folder  "/Trash")
+
+;; don't save message to Sent Messages, Gmail/IMAP takes care of this
+(setq mu4e-sent-messages-behavior 'delete)
+
+;; enable inline images
+(setq mu4e-view-show-images t)
+
+;; setup some handy shortcuts
+;; you can quickly switch to your Inbox -- press ``ji''
+;; then, when you want archive some messages, move them to
+;; the 'All Mail' folder by pressing ``ma''.
+
+(setq mu4e-maildir-shortcuts
+      '( ("/INBOX" . ?i)
+         ("/Sent" . ?s)
+         ("/Trash" . ?t)))
+
+;; allow for updating mail using 'U' in the main view:
+(setq mu4e-get-mail-command "offlineimap")
+
+(require 'mu4e-contrib)
+(setq mu4e-html2text-command 'mu4e-shr2text)
+
+;; something about ourselves
+(add-hook 'mu4e-compose-pre-hook
+  (defun my-set-from-address ()
+    "Set the From address based on the To address of the original."
+    (let ((msg mu4e-compose-parent-message)) ;; msg is shorter...
+      (when msg
+	(setq user-mail-address
+	  (cond
+	    ((mu4e-message-contact-field-matches msg :to "o.klimenko@2gis.ru")
+             "o.klimenko@2gis.ru")
+	    ((mu4e-message-contact-field-matches msg :to "heilage.nsk@gmail.com")
+             "heilage.nsk@gmail.com")
+            ((mu4e-message-contact-field-matches msg :to "klimenkoov@gmail.com")
+             "klimenkoov@gmail.com")
+	    (t "me@ctizen.net")))))
+    (setq
+     user-full-name  "Oleg Klimenko"
+     mu4e-compose-signature
+     (concat
+      "Oleg Klimenko\n"
+      "Email: " user-mail-address "\n"
+      "Mobile: +7 913 725 6906\n"
+      "\n"))
+    
+    ))
+
+(require 'smtpmail)
+(setq message-send-mail-function 'smtpmail-send-it
+    smtpmail-stream-type 'ssl
+    smtpmail-default-smtp-server "smtp.zoho.com"
+    smtpmail-smtp-server "smtp.zoho.com"
+    smtpmail-smtp-service 465)
+
+;; don't keep message buffers around
+(setq message-kill-buffer-on-exit t)
---
+++ .emacs.d/mu4e/mu4e-meta.el
@@ -0,0 +1,11 @@
+;; auto-generated
+(defconst mu4e-mu-version "0.9.16"
+  "Required mu binary version; mu4e's version must agree with this.")
+
+(defconst mu4e-builddir "/Users/heilage/Downloads/mu-0.9.16"
+  "Top-level build directory.")
+
+(defconst mu4e-doc-dir "/usr/local/share/doc/mu"
+  "Mu4e's data-dir.")
+
+(provide 'mu4e-meta)
