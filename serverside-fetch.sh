43adf4db396c1c7e731d948e1297631df60c47c5
Oleg Klimenko <o.klimenko@2gis.ru>
2016-09-24 13:58:00 +0700

    Add serverside fetch

--- serverside-fetch.sh
+++ serverside-fetch.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-cd ~/Mail 
-git add --all . 
-git commit -m 'User update' 
+offlineimap
+
+cd /home/notepad/Mail
+git add --all .
+git commit -m 'System update'
 if [ $? -eq 0 ]; then
     git fetch
-    echo "Fetch ok"
+    echo "Fetched ok"
     git rebase remotes/origin/master
     echo "Rebase ok"
     git push origin master
     echo "Push ok"
 else
     git pull -r origin master
-    echo "Pull ok"
+    echo "Pulled ok"
 fi
-
+q
