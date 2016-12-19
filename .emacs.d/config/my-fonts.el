76048b09b6655ac99869d7b89b32cbd28bbfce71
Oleg Klimenko <o.klimenko@2gis.ru>
2016-12-19 16:25:30 +0700

    Update emacs fonts

--- .emacs.d/config/my-fonts.el
+++ .emacs.d/config/my-fonts.el
@@ -1,13 +1,13 @@
 ;; Fonts
 (defvar *selected-font* (if (eq system-type 'darwin)
                             "Roboto Mono Light for Powerline-12"
-                          "Terminus-11"))
+                          "PragmataPro for Powerline-12"))
 (defvar *selected-font-family* (if (eq system-type 'darwin)
                                    "Roboto Mono Light for Powerline"
-                                 "Terminus"))
+                                 "PragmataPro for Powerline"))
 (set-frame-font *selected-font*)
 (set-face-attribute 'default t :font *selected-font* )
 (set-frame-font *selected-font* nil t)
 (if (eq system-type 'darwin)
     (add-to-list 'default-frame-alist '(font . "Roboto Mono Light for Powerline-12"))
-    (add-to-list 'default-frame-alist '(font . "Terminus-11")))
+    (add-to-list 'default-frame-alist '(font . "PragmataPro for Powerline-12")))
