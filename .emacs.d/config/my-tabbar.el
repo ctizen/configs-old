e69195db9961dc3b45af0570d4d13b37d6696b7e
Oleg Klimenko <o.klimenko@2gis.ru>
2016-12-19 16:30:56 +0700

    Update emacs tabbar

--- .emacs.d/config/my-tabbar.el
+++ .emacs.d/config/my-tabbar.el
@@ -65,7 +65,16 @@ That is, a string used to represent it on the tab bar."
                        (length (tabbar-view
                                 (tabbar-current-tabset)))))))))
 
-(tabbar-mode 1)
-
 (global-set-key [M-s-left] 'tabbar-backward)
 (global-set-key [M-s-right] 'tabbar-forward)
+
+(defun my-tabbar-buffer-groups () ;; customize to show all normal files in one group
+  "Returns the name of the tab group names the current buffer belongs to.
+  There are two groups: Emacs buffers (those whose name starts with '*', plus
+  dired buffers), and the rest.  This works at least with Emacs v24.2 using
+  tabbar.el v1.7."
+  (list (cond ((string-equal "*" (substring (buffer-name) 0 1)) "emacs")
+              ((eq major-mode 'dired-mode) "emacs")
+              (t "user"))))
+(setq tabbar-buffer-groups-function 'my-tabbar-buffer-groups)
+
