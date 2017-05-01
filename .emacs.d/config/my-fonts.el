6032629fbbdd4cb057a9f61779ac94da76e89da7
Oleg Klimenko <o.klimenko@2gis.ru>
2017-05-01 20:05:06 +0300

    Update emacs config

--- .emacs.d/config/my-fonts.el
+++ .emacs.d/config/my-fonts.el
@@ -176,12 +176,11 @@
   (prettify-symbols-mode -1)
   (prettify-symbols-mode +1))
 
-(setq prettify-symbols-unprettify-at-point 'right-edge)
-
 ;; Hooks for modes in which to install the Pragmata ligatures
 (mapc (lambda (hook)
         (add-hook hook (lambda () (setup-pragmata-ligatures) (refresh-pretty))))
       '(text-mode-hook
         prog-mode-hook))
+
 (global-prettify-symbols-mode +1)
 
